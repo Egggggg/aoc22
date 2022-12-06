58262431ecf939116f8f49a71d526b9b8ccf565f
Bee Clark <mrhumanman2@gmail.com>
2022-12-06 06:03:55 +0000

    Day 6

---
+++ day6/src/bin/part2.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let file = std::fs::read("input.txt").unwrap();
+    let mut current = [0u8; 14];
+    let mut found_at: usize = 0;
+
+    for (i, c) in file.iter().enumerate() {
+        current.rotate_right(1);
+        current[0] = *c;
+
+        let mut existing: Vec<u8> = Vec::new();
+        let mut repeat = false;
+
+        for c in current {
+            if existing.contains(&c) || c == 0 {
+                repeat = true;
+                break;
+            }
+
+            existing.push(c);
+        }
+
+        if !repeat {
+            found_at = i + 1; // add one cause 1 indexed
+            break;
+        }
+    }
+
+    println!("First marker: {found_at}");
+}
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,1 @@
-fn main() {
-    let file = std::fs::read("input.txt").unwrap();
-}
+fn main() {}
